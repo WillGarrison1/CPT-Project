0547a0986061f6cab8819e59e5492032b18929fe
Lincoln Hoffman <lincolnhoffman@outlook.com>
2025-02-14 11:32:32 -0600

    idk what this is

--- src/Physics/Collision.h
+++ src/Physics/Collision.h
@@ -35,8 +35,24 @@ namespace Engine
         if(!hasCollider1 || !hasCollider2) return false;
 
         float w1, h1, w2, h2;
-        Material *mat = static_cast<Material*>(e1.getComponent<ComponentID::Material>())
-        SDL_GetTextureSize(.texture, &w1, &h1);
+        Material *mat1 = static_cast<Material*>(e1.getComponent<ComponentID::Material>());
+        Material *mat2 = static_cast<Material*>(e2.getComponent<ComponentID::Material>());
+        SDL_GetTextureSize(mat1->texture, &w1, &h1);
+        SDL_GetTextureSize(mat1->texture, &w2, &h2);
         
+        Transform *tran1 = static_cast<Transform*>(e1.getComponent<ComponentID::Transform>());
+        Transform *tran2 = static_cast<Transform*>(e2.getComponent<ComponentID::Transform>());
+        Vector2<float> pos1 = tran1->position;
+        Vector2<float> pos2 = tran2->position;
+
+        if(pos1.x + w1 > pos2.x && pos1.y > pos2.y+h2
+           && pos1.x < pos2.x+w2 && pos1.y + h1 > pos1.y)
+        {
+            return true;
+        }
+
+        return false;
     }
+
+    
 }
